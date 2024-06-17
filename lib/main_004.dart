87bc0c3f34ea854774b930bb45aeeee3194b8421
hasimoto820 <hashimotokoji9999@gmail.com>
2024-06-17 14:19:48 +0900

    first

---
+++ lib/main_004.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import 'package:life_counter/life_event.dart';
+import 'package:life_counter/objectbox.g.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const LifeCounterPage(),
+    );
+  }
+}
+
+class LifeCounterPage extends StatefulWidget {
+  const LifeCounterPage({super.key});
+
+  @override
+  State<LifeCounterPage> createState() => _LifeCounterPageState();
+}
+
+class _LifeCounterPageState extends State<LifeCounterPage> {
+  Store? store;
+  Box<LifeEvent>? lifeEventBox;
+  List<LifeEvent> lifeEvents = [];
+
+  Future<void> initialize() async {
+    store = await openStore();
+    lifeEventBox = store?.box<LifeEvent>();
+    lifeEvents = lifeEventBox?.getAll() ?? [];
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initialize();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
